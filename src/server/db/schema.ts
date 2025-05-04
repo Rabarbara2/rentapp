f687f0d21872ecac1a84989bd884b604a770fee5
Toster <ebebebeawdawdaw@gmail.com>
2025-05-05 01:23:20 +0200

    .

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -265,14 +265,11 @@ export const maintenance_requestRelations = relations(
 );
 export const viewing_request = createTable("viewing_request", (d) => ({
   id: d.integer().notNull().primaryKey(),
-  listing_id: d
-    .integer()
-    .notNull()
-    .references(() => listing.id, {
+  listing_id: d.integer().notNull(),
+  /*.references(() => listing.id, {
       onDelete: "cascade",
       onUpdate: "cascade",
-    }),
-  tenant_id: d
+    })*/ tenant_id: d
     .varchar({ length: 255 })
     .notNull()
     .references(() => user.id, {
@@ -328,81 +325,5 @@ export const propertyRelations = relations(property, ({ one, many }) => ({
 }));
 
 //--------------------------------------------------------------------
-
-export const users_old = createTable("user_old", (d) => ({
-  id: d
-    .varchar({ length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: d.varchar({ length: 255 }),
-  email: d.varchar({ length: 255 }).notNull(),
-  emailVerified: d
-    .timestamp({
-      mode: "date",
-      withTimezone: true,
-    })
-    .default(sql`CURRENT_TIMESTAMP`),
-  image: d.varchar({ length: 255 }),
-}));
-
-export const usersRelations = relations(user, ({ many }) => ({
-  accounts: many(accounts),
-}));
-
-export const accounts = createTable(
-  "account",
-  (d) => ({
-    userId: d
-      .varchar({ length: 255 })
-      .notNull()
-      .references(() => user.id),
-    type: d.varchar({ length: 255 }).$type<AdapterAccount["type"]>().notNull(),
-    provider: d.varchar({ length: 255 }).notNull(),
-    providerAccountId: d.varchar({ length: 255 }).notNull(),
-    refresh_token: d.text(),
-    access_token: d.text(),
-    expires_at: d.integer(),
-    token_type: d.varchar({ length: 255 }),
-    scope: d.varchar({ length: 255 }),
-    id_token: d.text(),
-    session_state: d.varchar({ length: 255 }),
-  }),
-  (t) => [
-    primaryKey({ columns: [t.provider, t.providerAccountId] }),
-    index("account_user_id_idx").on(t.userId),
-  ],
-);
-
-export const accountsRelations = relations(accounts, ({ one }) => ({
-  user: one(user, { fields: [accounts.userId], references: [user.id] }),
-}));
-
-export const sessions = createTable(
-  "session",
-  (d) => ({
-    sessionToken: d.varchar({ length: 255 }).notNull().primaryKey(),
-    userId: d
-      .varchar({ length: 255 })
-      .notNull()
-      .references(() => user.id),
-    expires: d.timestamp({ mode: "date", withTimezone: true }).notNull(),
-  }),
-  (t) => [index("t_user_id_idx").on(t.userId)],
-);
-
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(user, { fields: [sessions.userId], references: [user.id] }),
-}));
-
-export const verificationTokens = createTable(
-  "verification_token",
-  (d) => ({
-    identifier: d.varchar({ length: 255 }).notNull(),
-    token: d.varchar({ length: 255 }).notNull(),
-    expires: d.timestamp({ mode: "date", withTimezone: true }).notNull(),
-  }),
-  (t) => [primaryKey({ columns: [t.identifier, t.token] })],
-);
 
 export type UsersType = typeof user.$inferInsert;
